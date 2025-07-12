f1035f85269e9303c34bd88151ae734dac7fb566
Gautham <gauthamsritharan@gmail.com>
2025-07-12 07:16:15 +0200

    Commit 3 - removed API keys

--- local_config.py
+++ local_config.py
@@ -1,8 +1,8 @@
 # config.py
 
-OPENAI_API_KEY = "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"
-COHERE_API_KEY = "IMJR3hvqtZt4nMJoh0hWx6NBwlg0dd16eFlRmEiP"
-VOYAGE_API_KEY = "pa-OQpxm9GsD1Pr1NqvrfmBc_-sZ_py-uf7uQbTTYl4bwQ"
+OPENAI_API_KEY = ""
+COHERE_API_KEY = ""
+VOYAGE_API_KEY = ""
 
 FUNCTION_JSON_PATH = "outputs/codenetpy_functions.jsonl"
 EMBEDDINGS_DIR = "embeddings"
